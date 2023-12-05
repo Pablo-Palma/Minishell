d77a5520c03ed61ff8ba84420a1ce2fdc13d60e4
jimmy <jbaeza-c@student.42madrid.com>
2023-12-05 20:36:51 +0100

    main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jbaeza-c <jbaeza-c@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 19:34:21 by jbaeza-c          #+#    #+#             */
-/*   Updated: 2023/12/05 19:40:08 by jbaeza-c         ###   ########.fr       */
+/*   Updated: 2023/12/05 20:36:40 by jbaeza-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(int argc, char **argv, char **envp)
 {
 	if (argc == 1)
-		minishell(argv, envp);
+		minishell(envp);
 	else
 		ft_printf("Incorrect input. Please, execute as follows: ./minishell\n");
 	return (0);
