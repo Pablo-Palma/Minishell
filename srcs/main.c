576237df50109c372fd7ffbe70eb7aefad6bd21c
jimmy <jbaeza-c@student.42madrid.com>
2023-12-05 19:36:54 +0100

    Created main.c

---
+++ srcs/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbaeza-c <jbaeza-c@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 19:34:21 by jbaeza-c          #+#    #+#             */
+/*   Updated: 2023/12/05 19:36:34 by jbaeza-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incl/minishell.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	if (argc == 1)
+		minishell(argv, envp);
+	else
+		ft_printf("Incorrect input. Please, execute as follows: ./minishell\n");
+	return (0);
+}
