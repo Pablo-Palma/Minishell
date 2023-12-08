ad7b011bdcb4a449f8f232185ff8af16e80df1d2
jimmy <jbaeza-c@student.42madrid.com>
2023-12-08 13:24:29 +0100

    Pipe working but not executing properly

--- src/input/input.c
+++ src/input/input.c
@@ -6,7 +6,7 @@
 /*   By: jbaeza-c <jbaeza-c@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 22:53:13 by jbaeza-c          #+#    #+#             */
-/*   Updated: 2023/12/07 03:00:21 by jbaeza-c         ###   ########.fr       */
+/*   Updated: 2023/12/07 21:42:38 by jbaeza-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,12 @@ int	handle_input(char *input, t_minishell *shell) //SIN REDIRECCIONES
 	shell->commands = ft_split(input, '|');
 	shell->number_commands = ft_tablen(shell->commands);
 	if (shell->number_commands == 1)
+	{
+		//ft_printf("Ejecutando comandos sin pipe\n");
 		execute_command(shell->commands[i], shell);
+	}
 	else
-		execute_pipe_command(shell);
+		if (execute_pipe_command(shell) == -1)
+			ft_printf("ERROR EN EXEC_PIPE_COMMAND");
 	return (1);
 }
--- src/pipe/pipe.c
+++ src/pipe/pipe.c
@@ -6,7 +6,7 @@
 /*   By: jbaeza-c <jbaeza-c@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 03:00:42 by jbaeza-c          #+#    #+#             */
-/*   Updated: 2023/12/07 03:28:46 by jbaeza-c         ###   ########.fr       */
+/*   Updated: 2023/12/07 21:42:05 by jbaeza-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,19 @@
 
 void	child(t_minishell *shell, int *fd)
 {
-	int	infile;
-	
-	infile = open("Makefile", O_RDONLY, 0777);
 	dup2(fd[1], STDOUT_FILENO);
-	dup2(infile, STDIN_FILENO);
 	close(fd[0]);
+	close(fd[1]);
+	//ft_printf("Primer comando a ejecutar: %s\n", shell->commands[0]);
 	execute_command(shell->commands[0], shell);
 }
 
 void	parent(t_minishell *shell, int *fd)
 {
 	dup2(fd[0], STDIN_FILENO);
-	dup2(1, STDOUT_FILENO);
 	close(fd[1]);
+	close(fd[0]);
+	//ft_printf("Segundo comando a ejecutar: %s\n", shell->commands[1]);
 	execute_command(shell->commands[1], shell);
 }
 
@@ -42,8 +41,12 @@ int	execute_pipe_command(t_minishell *shell)
 	if (pid == -1)
 		return (-1);
 	if (!pid)
+	{
+		//ft_printf("Entrando a child process\n");
 		child(shell, fd);
+	}
 	waitpid(pid, NULL, 0);
+	//ft_printf("Entrando a parent process\n");
 	parent(shell, fd);
 	return (1);
 }
