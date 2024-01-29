e5896f8f0c77c494a2542b5540e0425566a2c608
Pablo Palma Rodríguez <pablopalmarodriguez@iMac-de-Pablo.local>
2024-01-29 16:18:29 +0100

    working on complex wildcards

--- src/bonus/wildcards.c
+++ src/bonus/wildcards.c
@@ -6,14 +6,38 @@
 /*   By: pabpalma <pabpalma>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 11:14:03 by pabpalma          #+#    #+#             */
-/*   Updated: 2024/01/29 15:27:45 by pabpalma         ###   ########.fr       */
+/*   Updated: 2024/01/29 16:16:54 by pabpalma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <dirent.h>
 #include <minishell.h>
 
-//int	match_pattern
+int	match_pattern(const char *filename, const char *pattern)
+{
+	int	pat_len = ft_strlen(pattern);
+	int	filename_len = ft_strlen(filename);
+	char	*star = ft_strchr(pattern, '*');
+	int	prefix_len = star - pattern;
+	int	suffix_len = pat_len - prefix_len - 1;
+
+	if (filename[0] == '.')
+		return (0);
+	if (ft_strncmp(pattern, "*", 2) == 0)
+		return(1);
+	if (pattern[pat_len - 1] == '*')
+        return ft_strncmp(filename, pattern, pat_len - 1) == 0;
+	if (pattern[0] == '*')
+		return ft_strncmp(filename + filename_len - (pat_len - 1), pattern + 1, pat_len - 1) == 0;
+	else
+	{
+		if (prefix_len > filename_len || suffix_len > filename_len)
+			return (0);
+		return ft_strncmp(filename, pattern, prefix_len) == 0 &&
+				ft_strncmp(filename + filename_len - suffix_len + 1, star + 1,suffix_len) == 0;
+	}
+	//return ft_strncmp(filename, pattern, pat_len) == 0;
+}
 
 int	count_elements(char **array)
 {
@@ -24,7 +48,7 @@ int	count_elements(char **array)
 	return (count);
 }
 
-int	count_files(void)
+int	count_files(char *pattern)
 {
 	DIR				*dir;
 	struct dirent	*entry;
@@ -38,7 +62,7 @@ int	count_files(void)
 	}
 	while ((entry = readdir(dir)) != NULL)
 	{
-		if (entry->d_name[0] != '.')
+		if (match_pattern(entry->d_name, pattern))
 			count++;
 	}
 	closedir(dir);
@@ -53,8 +77,10 @@ char	**command(char **args, char **files)
 	int		count = count_elements(args) + count_elements(files);
 
 	command = malloc(sizeof(char *) * (count));
-	while (args[i] && ft_strncmp(args[i], "*", 2) != 0)
+	while (args[i])
 	{
+		if (ft_strchr(args[i], '*'))
+			break ;
 		command[i] = ft_strdup(args[i]);
 		free(args[i]);
 		i++;
@@ -75,19 +101,25 @@ char	**expand_wildcards(char **args)
 {
 	DIR				*dir;
 	struct dirent	*entry;
+	char			*pattern = NULL;
 	char			**files;
 	int				i = 0;
 	int				count = 0;
 
 	while(args[i])
 	{
-		if (ft_strncmp(args[i], "*", 1) == 0)
+		if (ft_strchr(args[i], '*'))
+		{
+			pattern = args[i];
 			break;
+		}
 		i++;
 	}
+	if (!pattern)
+		return (NULL);
 	if (args[i] == NULL)
 		return (NULL);
-	count = count_files();
+	count = count_files(pattern);
 	files = malloc(sizeof(char *) * (count + 1));
 	if (!files)
 		return (NULL);
@@ -100,7 +132,7 @@ char	**expand_wildcards(char **args)
 	i = 0;
 	while ((entry = readdir(dir)) != NULL)
 	{
-		if (entry->d_name[0] != '.')
+		if (match_pattern(entry->d_name, pattern))
 		{
 			files[i] = ft_strdup(entry->d_name);
 			i++;
