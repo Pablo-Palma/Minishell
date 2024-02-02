9c6e855ab18a2539c5d78bdc569f6aa4f06c1632
jimmy <jbaeza-c@student.42madrid.com>
2024-02-02 22:14:07 +0100

    Open brackets have been handled

--- tester/tester.sh
+++ tester/tester.sh
@@ -4,18 +4,19 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 LIGHT_BLUE='\033[1;34m'
+PURPLE='\033[01;35m'
 NC='\033[0m'
 
 check_command() {
 	test_id=$1
     command=$2
-	echo -e "${YELLOW}Executing Test #${test_id}: $command${NC}"
+	echo -e "${YELLOW}Executing Test #${NC} ${PURPLE}${test_id}${NC} ${YELLOW}: $command${NC}"
 
     bash -c "$command" > bash_output.txt
     ./minishell -c "$command" > minishell_output.txt
 
     if diff -q bash_output.txt minishell_output.txt > /dev/null; then
-		echo -e "${GREEN}OK: Test #${test_id}${NC}"
+		echo -e "${RED}KO: Test #${test_id}${NC}"
     else
 		echo -e "${GREEN}OK: Test #${test_id}${NC}"
     fi
@@ -44,6 +45,9 @@ basic_commands=(
 	"/usr/bin/find /etc -type f -name 'hosts'"
 	"/bin/cat /etc/hosts"
 	"echo $(date)"
+    "/bin/cat Makefile"
+    "/bin/ls Makefile"
+    "/bin/ls Makefile -l"
 )
 
 for cmd in "${basic_commands[@]}"; do
@@ -78,6 +82,11 @@ sequential_commands=(
     '(nonexistentcommand && echo "B") || echo "Fallback"'
     'true && false || echo "Recovered"'
 	'echo 1 && (echo 2 || echo 3) | echo 4'
+    'echo 1 && (echo 2 && (echo 3 && echo4) && echo 5)'
+    'echo 1 && (echo 2 && (echo 3 && echo4) || echo 5)'
+    'echo 1 && (echo 2 && (echo 3 || echo4) && echo 5)'
+    'echo 1 && (echo 2 || (echo 3 && echo4) && echo 5)'
+    'echo 1 && (echo 2 || (echo 3 || echo4) && echo 5)'
 )
 
 for cmd in "${sequential_commands[@]}"; do
